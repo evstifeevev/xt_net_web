56dc21c3f1b6151cc813a52131bb1558f9458b13
Evgeniy Evstifeev <evstifeevev@mail.ru>
2020-02-05 22:55:16 +0400

    Minor bug fixes in Task 9.2.

--- EvstifeevEvgeniyTasks/Task9/Subtask 2 Math calculator/Scripts/mathCalculator.js
+++ EvstifeevEvgeniyTasks/Task9/Subtask 2 Math calculator/Scripts/mathCalculator.js
@@ -1,9 +1,44 @@
 var mathCalculatorWrapper = {
     reOperators: /\+|\*|\-|\/|\=/g,
     mathCalculator:function (line){
+        if(line==undefined){
+            console.error("Error: The mathemathical expression was empty.", line);
+            return;
+        }
+        if(line.match(/((^(\*|\/))|(\-|\+|\*)\/)|((\-|\+|\/)\*)/g)!=null){
+            console.error("Error: Wrong mathematical expression. ");
+            return;
+        }
+        line = line.replace(/( *\-( *|\+*)\- *)/g,"+");
+        line = line.replace(/(\+){2,}/g,"+");
+        line = line.replace(/(\/\+){1,}/g,"/");
+        line = line.replace(/(\*\+){1,}}/g,"*");
+        line = line.replace(/(\*){2,}/g,"*");
+        line = line.replace(/(\/){2,}/g,"/");
         var numbers = line.split(this.reOperators);
         var operators = line.match(this.reOperators);
+        numbers.forEach(item =>{
+            document.writeln(item);
+        })
+        if(operators==null)
+        {
+            operators=[];
+            operators.push('=');    
+        }
+        
+            operators.forEach(item =>{
+                document.writeln(item);
+            })  
+        
+        var find = operators.find(function(element){return element=='='});
+        if(find==undefined || find==null){
+            operators.push('=');
+        }
+         
         var result = operators.length>1? +numbers[0] : 0;
+        if(operators.length==1 && operators[0]=='='){
+            result=numbers[0];
+        }
         for(var operationIndex=0;operationIndex<operators.length-1;
             operationIndex++){
             switch(operators[operationIndex]){
@@ -22,8 +57,8 @@ var mathCalculatorWrapper = {
             }
         }
         result=Math.round(result*100)/100;
-		document.write(line);
-        document.write(result);
+        document.writeln(result);
+        console.log(result);
     },
 };
-mathCalculatorWrapper.mathCalculator("3 + 3 * 3 / 4 - 3 * 10e5 = ")
+mathCalculatorWrapper.mathCalculator("3 + 3 * 3 / 4 - 3 * 10e5")
